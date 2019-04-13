d321d21199beb86c137b8ae5bd4aac26d9b9fcfd
Samir AMARA <samirq1@outlook.com>
2019-04-13 14:11:07 +0200

    api hébérgé

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // URL of production API
-  apiUrl: 'http://localhost:8000'
+  apiUrl: 'http://fenouiladminpanel.tk/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   // URL of development API
-  apiUrl: 'http://localhost:8000'
+  apiUrl: 'http://fenouiladminpanel.tk/'
 };
